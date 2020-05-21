5e117b6c7de73150c496c2df66d67a4b8a0b6b08
surenkid <surenkids@gmail.com>
2020-05-21 09:33:58 +0800

    Change coscmd.sh for Bourne shell (bin/sh)

--- volume/coscmd/coscmd.sh
+++ volume/coscmd/coscmd.sh
@@ -1,22 +1,30 @@
 #!/bin/sh
-backup_list[0]="/mnt/nfs1/volumes"
-backup_list[1]="/mnt/nfs2/upload/static.sumeils.com/2020"
-backup_list[2]="/mnt/nfs2/upload/truly-mall.sumeils.com"
-
 upload_dir=/$(date "+%Y/%m/%d")
-delete_dir=/$(date -d"-3 month +1 day" +%Y-%m-%d)
+delete_dir=/$(date --date="@$(($(date +%s) - 3600*24*90))" "+%Y/%m/%d")
 
-# list and zip folders
-for path in ${backup_list[@]}
+# backup folders
+set "/nfs1" "/nfs2/upload/static.sumeils.com/2020" "/nfs2/upload/truly-mall.sumeils.com"
+for path in do "$@"
+do
+	tar cvzf /archive.tar.gz $path
+	/usr/local/bin/coscmd upload /archive.tar.gz $upload_dir$path/archive.tar.gz -H "{'x-cos-storage-class':'Archive'}"
+	rm -f /archive.tar.gz
+done
+set x; shift
+
+# backup sub folders
+set "/nfs1" "/nfs2/upload/static.sumeils.com/2020" "/nfs2/upload/truly-mall.sumeils.com"
+for path in do "$@"
 do
 	dirList=`ls -p $path |grep / |tr -d /`
 	for dir in $dirList
 	do
 		tar cvzf /$dir.tar.gz $path/$dir
-		/usr/local/bin/coscmd upload /$dir.tar.gz $upload_dir -H "{'x-cos-storage-class':'Archive'}"
+		/usr/local/bin/coscmd upload /$dir.tar.gz $upload_dir$path/$dir.tar.gz -H "{'x-cos-storage-class':'Archive'}"
 		rm -f /$dir.tar.gz
 	done
 done
+set x; shift
 
 # delete backup files before 3 month
 /usr/local/bin/coscmd delete -rf $delete_dir

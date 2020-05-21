39bc98282c9ea7e9b42ebd436673a667853ae8ef
surenkid <surenkids@gmail.com>
2020-05-21 15:03:15 +0800

    Fix ftp pasv_address default value

--- dockerfile/docker-ftp/docker-entrypoint.sh
+++ dockerfile/docker-ftp/docker-entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 if [ -z ${PASSWORD} ]; then
-  PASSWORD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c${1:-8};echo;)
+  PASSWORD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c${1:-16};echo;)
   echo "Generated password for user 'ktsee': ${PASSWORD}"
 fi
 
 # set passive mode parameters:
+if [ "$PASV_ADDRESS" = "**IPv4**" ]; then
+    export PASV_ADDRESS=$(/sbin/ip route|awk '/default/ { print $3 }')
+fi
 echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf

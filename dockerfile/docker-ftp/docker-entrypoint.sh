596d61faa0d6cbc5f6d8e76daff9dbe909a02d9c
surenkid <surenkids@gmail.com>
2020-05-21 11:53:05 +0800

    Fix home dir for vsftpd

--- dockerfile/docker-ftp/docker-entrypoint.sh
+++ dockerfile/docker-ftp/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 if [ -z ${PASSWORD} ]; then
   PASSWORD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c${1:-8};echo;)
-  echo "Generated password for user 'ftp': ${PASSWORD}"
+  echo "Generated password for user 'ktsee': ${PASSWORD}"
 fi
 # set ftp user password
 echo "ktsee:${PASSWORD}" |/usr/sbin/chpasswd
-chown ktsee:ktsee /home/ktsee/ -R
+chown ktsee:ktsee /home/ -R
 
 if [ -z $1 ]; then
   /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf

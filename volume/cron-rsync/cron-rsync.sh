1b663c38e5c655d801d738ed7c150bc120a295b2
surenkid <surenkids@gmail.com>
2020-05-27 15:30:48 +0800

    Fix cron-rsync shell script error

--- volume/cron-rsync/cron-rsync.sh
+++ volume/cron-rsync/cron-rsync.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 local_dir=/var/www/html/
 code_dir=/root/code/
-upload_dir=/root/source/
+upload_dir=/root/upload/
 
 # Pull code from remote
 rsync -avzhupgo --progress $code_dir $local_dir >> /root/rsync/pull.log
 
 # Push user generate content to remote
-rsync -avzhupgo --progress —include-from="/root/rsync/upload_dir.txt" —exclude="*" $local_dir $upload_dir >> /root/rsync/push.log
+rsync -avzhupgo --progress --include-from="/root/rsync/upload_dir.conf" --exclude="/*" $local_dir $upload_dir >> /root/rsync/push.log

a9faeafac3530ff016a11ddd30838c35af553d07
surenkid <surenkids@gmail.com>
2020-05-31 21:55:33 +0800

    Fix entrypoint.sh backgroud bug

--- dockerfile/docker-cron-rsync/scripts/docker-entrypoint.sh
+++ dockerfile/docker-cron-rsync/scripts/docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-crond
+
 if [ -z $1 ]; then
   sh /root/inotify-rsync-push.sh ${UPLOAD_SUB_FOLDER}
 else
   $@
 fi
+
+crond -f
--- dockerfile/docker-cron-rsync/scripts/inotify-rsync-push.sh
+++ dockerfile/docker-cron-rsync/scripts/inotify-rsync-push.sh
@@ -21,9 +21,6 @@ do
                 echo 'CREATE or MODIFY or CLOSE_WRITE or MOVED_TO'
                 path=$(dirname ${INO_FILE})
                 echo ${path:${#local_dir}} >> /root/pre-push-file-list.ktsee
-                #rsync -avzcR --password-file=${rsync_passwd_file} $(dirname ${INO_FILE}) ${user}@${ip1}::${des} &&         # INO_FILE变量代表路径哦  -c校验文件内容
-                #rsync -avzcR --password-file=${rsync_passwd_file} $(dirname ${INO_FILE}) ${user}@${ip2}::${des}
-                 #仔细看 上面的rsync同步命令 源是用了$(dirname ${INO_FILE})变量 即每次只针对性的同步发生改变的文件的目录(只同步目标文件的方法在生产环境的某些极端环境下会漏文件 现在可以在不漏文件下也有不错的速度 做到平衡) 然后用-R参数把源的目录结构递归到目标后面 保证目录结构一致性
         fi
         #删除、移动出事件
         if [[ "$CHECK_DELETE" != "" ]] || [[ "$CHECK_MOVED_FROM" != "" ]]
@@ -31,9 +28,6 @@ do
                 echo 'DELETE or MOVED_FROM'
                 path=$(dirname ${INO_FILE})
                 echo ${path:${#local_dir}} >> /root/pre-push-file-list.ktsee
-                #rsync -avzR --delete --password-file=${rsync_passwd_file} $(dirname ${INO_FILE}) ${user}@${ip1}::${des} &&
-                #rsync -avzR --delete --password-file=${rsync_passwd_file} $(dirname ${INO_FILE}) ${user}@${ip2}::${des}
-                #看rsync命令 如果直接同步已删除的路径${INO_FILE}会报no such or directory错误 所以这里同步的源是被删文件或目录的上一级路径，并加上--delete来删除目标上有而源中没有的文件，这里不能做到指定文件删除，如果删除的路径越靠近根，则同步的目录月多，同步删除的操作就越花时间。这里有更好方法的同学，欢迎交流。
         fi
         #修改属性事件 指 touch chgrp chmod chown等操作
         if [[ "$CHECK_ATTRIB" != "" ]]
@@ -44,8 +38,6 @@ do
                         echo 'ATTRIB FILE'
                         path=$(dirname ${INO_FILE})
                         echo ${path:${#local_dir}} >> /root/pre-push-file-list.ktsee
-                        #rsync -avzcR --password-file=${rsync_passwd_file} $(dirname ${INO_FILE}) ${user}@${ip1}::${des} &&
-                        #rsync -avzcR --password-file=${rsync_passwd_file} $(dirname ${INO_FILE}) ${user}@${ip2}::${des}
                 fi
         fi
 done

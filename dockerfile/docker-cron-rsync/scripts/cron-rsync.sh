d727843a59c357c2a4c37bdc1a30f972a7e3b5d0
surenkid <surenkids@gmail.com>
2020-05-31 22:41:47 +0800

    Fix cron-rsync shell script

--- dockerfile/docker-cron-rsync/scripts/cron-rsync.sh
+++ dockerfile/docker-cron-rsync/scripts/cron-rsync.sh
@@ -2,13 +2,14 @@
 local_dir=/var/www/html/
 remote_dir=/root/remote/
 
+
 # === pull files ===
 # check last pull and last push
 remote_push_time=`cat /root/remote/last-part-push.ktsee`
 local_pull_time=`cat /root/last-full-pull.ktsee`
-if [ ! -f "/root/remote/last-part-push.ktsee" ] || [ ! -f "/root/last-full-pull.ktsee" ] || [ $remote_push_time -gt $local_pull_time ]; then
+if [ ! -f /root/remote/last-part-push.ktsee ] || [ ! -f /root/last-full-pull.ktsee ] || [ $remote_push_time -gt $local_pull_time ]; then
     # Pull code from remote
-    rsync -avzhupgo $remote_dir $local_dir >> /proc/self/fd/2
+    rsync -avzupgo $remote_dir $local_dir >> /proc/self/fd/2
 
     # get current timestamp
     cur_sec_and_ns=`date '+%s-%N'`
@@ -21,7 +22,7 @@ if [ ! -f "/root/remote/last-part-push.ktsee" ] || [ ! -f "/root/last-full-pull.
 fi
 
 # === push files ===
-if [ -f "/root/pre-push-file-list.ktsee" ]; then
+if [ -f /root/pre-push-file-list.ktsee ]; then
     # remove repeat line
     sort /root/pre-push-file-list.ktsee | uniq > /root/push-file-list.ktsee
     rm -rf /root/pre-push-file-list.ktsee
@@ -29,8 +30,8 @@ if [ -f "/root/pre-push-file-list.ktsee" ]; then
     # copy changed path
     while read path
     do
-    rsync -avzhupgo --exclude=*/ $local_dir$path $remote_dir$path 
-    done < '/root/push-file-list.ktsee';
+        rsync -avzupgo --exclude=*/ $local_dir$path $remote_dir$path >> /proc/self/fd/2
+    done < /root/push-file-list.ktsee;
     rm -rf /root/push-file-list.ktsee
 
     # get current timestamp

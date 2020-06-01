42c7e2fccf4921d4161dea116a4e05220125bf4d
surenkid <surenkids@gmail.com>
2020-06-01 20:58:24 +0800

    Fix pull from remote to local error

--- dockerfile/docker-cron-rsync/scripts/cron-rsync.sh
+++ dockerfile/docker-cron-rsync/scripts/cron-rsync.sh
@@ -2,45 +2,45 @@
 local_dir=/var/www/html
 remote_dir=/root/remote
 
-
 # === pull files from remote to local ===
+# get current timestamp
+cur_sec_and_ns=`date '+%s-%N'`
+cur_sec=${cur_sec_and_ns%-*}
+cur_ns=${cur_sec_and_ns##*-}
+cur_timestamp=$((cur_sec*1000+cur_ns/1000000))
+
 # check last pull and last push
 if [ ! -f /root/last-full-pull.ktsee ]; then
     echo 0000000000000 > /root/last-full-pull.ktsee
 fi
 
-if [ ! -f /root/remote/last-part-push.ktsee ]; then
-    # Pull code from remote fully
+if [ ! -f $remote_dir/last-part-push.ktsee ]; then
+    echo $cur_timestamp > $remote_dir/last-part-push.ktsee
+fi
+
+# check newer, if remote is newer, pull from remote to local
+remote_push_time=`cat /root/remote/last-part-push.ktsee`
+local_pull_time=`cat /root/last-full-pull.ktsee`
+if [ $remote_push_time -gt $local_pull_time ]; then
+    # Pull code from remote partly
     echo "Pull files from $remote_dir/ to $local_dir/"
-    rsync -avzupgoI $remote_dir/ $local_dir/ --exclude=last-part-push.ktsee >> /proc/self/fd/2
 
-    # get current timestamp
-    cur_sec_and_ns=`date '+%s-%N'`
-    cur_sec=${cur_sec_and_ns%-*}
-    cur_ns=${cur_sec_and_ns##*-}
-    cur_timestamp=$((cur_sec*1000+cur_ns/1000000))
-    echo $cur_timestamp > /root/remote/last-part-push.ktsee
-else
-    remote_push_time=`cat /root/remote/last-part-push.ktsee`
-    local_pull_time=`cat /root/last-full-pull.ktsee`
-    if [ $remote_push_time -gt $local_pull_time ]; then
-        # Pull code from remote partly
-        echo "Pull files from $remote_dir/ to $local_dir/"
-        rsync -avzupgo $remote_dir/ $local_dir/ --exclude=last-part-push.ktsee >> /proc/self/fd/2
-
-        # get current timestamp
-        cur_sec_and_ns=`date '+%s-%N'`
-        cur_sec=${cur_sec_and_ns%-*}
-        cur_ns=${cur_sec_and_ns##*-}
-        cur_timestamp=$((cur_sec*1000+cur_ns/1000000))
-
-        # update last pull time
-        echo $cur_timestamp > /root/last-full-pull.ktsee
-
-        # init inotify watch
-        flock -xn /tmp/inotify-rsync-push.lock -c "nohup sh /root/inotify-rsync-push.sh &" >> /proc/self/fd/2
-        touch /root/deploy.ktsee
+    # if first-pull.ktsee exist, pull partly
+    if [ -f $remote_dir/first-pull.ktsee]; then
+        rsync -avzupgo $remote_dir/ $local_dir/ --exclude=last-part-push.ktsee --exclude=first-pull.ktsee >> /proc/self/fd/2   
+    else
+        rsync -avzupgoI $remote_dir/ $local_dir/ --exclude=last-part-push.ktsee >> /proc/self/fd/2
+        touch $remote_dir/first-pull.ktsee
     fi
+
+    # update last pull time
+    echo $cur_timestamp > /root/last-full-pull.ktsee
+
+    # init inotify watch
+    flock -xn /tmp/inotify-rsync-push.lock -c "nohup sh /root/inotify-rsync-push.sh &" >> /proc/self/fd/2
+    touch /root/deploy.ktsee
+else
+    echo "No files need to pull"
 fi
 
 # === push files from local to remote ===

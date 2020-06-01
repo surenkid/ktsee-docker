6cfaeef80017f0a4fbdd016088ffc65efd61ec86
surenkid <surenkids@gmail.com>
2020-06-01 14:37:02 +0800

    Change rsync first sync to fully overwrite

--- dockerfile/docker-cron-rsync/scripts/cron-rsync.sh
+++ dockerfile/docker-cron-rsync/scripts/cron-rsync.sh
@@ -5,34 +5,39 @@ remote_dir=/root/remote
 
 # === pull files from remote to local ===
 # check last pull and last push
+if [ ! -f /root/last-full-pull.ktsee ]; then
+    echo 0000000000000 > /root/last-full-pull.ktsee
+fi
+
 if [ ! -f /root/remote/last-part-push.ktsee ]; then
+    # Pull code from remote fully
+    echo "Pull files from $remote_dir/ to $local_dir/"
+    rsync -avzupgoI $remote_dir/ $local_dir/ --exclude=last-part-push.ktsee >> /proc/self/fd/2
+
     # get current timestamp
     cur_sec_and_ns=`date '+%s-%N'`
     cur_sec=${cur_sec_and_ns%-*}
     cur_ns=${cur_sec_and_ns##*-}
     cur_timestamp=$((cur_sec*1000+cur_ns/1000000))
     echo $cur_timestamp > /root/remote/last-part-push.ktsee
-fi
-if [ ! -f /root/last-full-pull.ktsee ]; then
-    echo 0000000000000 > /root/last-full-pull.ktsee
-fi
+else
+    remote_push_time=`cat /root/remote/last-part-push.ktsee`
+    local_pull_time=`cat /root/last-full-pull.ktsee`
+    if [ $remote_push_time -gt $local_pull_time ]; then
+        # Pull code from remote partly
+        echo "Pull files from $remote_dir/ to $local_dir/"
+        rsync -avzupgo $remote_dir/ $local_dir/ --exclude=last-part-push.ktsee >> /proc/self/fd/2
 
-remote_push_time=`cat /root/remote/last-part-push.ktsee`
-local_pull_time=`cat /root/last-full-pull.ktsee`
-if [ $remote_push_time -gt $local_pull_time ]; then
-    # Pull code from remote
-    echo "Pull files from $remote_dir/ to $local_dir/"
-    rsync -avzupgo $remote_dir/ $local_dir/ --exclude=last-part-push.ktsee >> /proc/self/fd/2
+        # get current timestamp
+        cur_sec_and_ns=`date '+%s-%N'`
+        cur_sec=${cur_sec_and_ns%-*}
+        cur_ns=${cur_sec_and_ns##*-}
+        cur_timestamp=$((cur_sec*1000+cur_ns/1000000))
 
-    # get current timestamp
-    cur_sec_and_ns=`date '+%s-%N'`
-    cur_sec=${cur_sec_and_ns%-*}
-    cur_ns=${cur_sec_and_ns##*-}
-    cur_timestamp=$((cur_sec*1000+cur_ns/1000000))
-
-    # update last pull time
-    echo $cur_timestamp > /root/last-full-pull.ktsee
-    touch /root/deploy.ktsee
+        # update last pull time
+        echo $cur_timestamp > /root/last-full-pull.ktsee
+        touch /root/deploy.ktsee
+    fi
 fi
 
 # === push files from local to remote ===
